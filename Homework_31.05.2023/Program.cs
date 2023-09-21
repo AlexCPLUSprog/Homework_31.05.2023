d1179d7b0016d191cb856c145468cc2f17d2cb1e
Алекс <Alexandr_enid@mail.ru>
2023-09-21 20:26:03 +0500

    Добавьте файлы проекта.

---
+++ Homework_31.05.2023/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_31._05._2023
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FileSearch fileSearch = new FileSearch();
+            fileSearch.File_Search(fileSearch);
+        }
+    }
+}
